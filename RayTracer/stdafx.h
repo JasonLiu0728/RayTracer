ce5be09a1c4682d770a361e5dee52d7fbe494d5e
JasonLiu0728 <yuxi.liu2007@gmail.com>
2015-07-27 15:22:58 -0400

    Initial commit for the Windows platform

---
+++ RayTracer/stdafx.h
@@ -0,0 +1,28 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "util.h"
+#include "scene_object.h"
+#include "light_source.h"
+#include "bmp_io.h"
+#include "raytracer.h"
+
+
+
+
+// TODO: reference additional headers your program requires here
